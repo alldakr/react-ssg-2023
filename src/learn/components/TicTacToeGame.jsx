f514ca49888cb1518828479ca364ad1c7abe6728
yamoo9 <yamoo9@naver.com>
2023-10-26 18:58:22 +0900

    게임 인포메이션 컴포넌트 추가

--- src/learn/components/TicTacToeGame.jsx
+++ src/learn/components/TicTacToeGame.jsx
@@ -1,7 +1,7 @@
 import Stack from './Stack';
+import styles from './TicTacToeGame.module.css';
 import Board from './ticTacToe/Board';
 import History from './ticTacToe/History';
-import styles from './TicTacToeGame.module.css';
 
 function TicTacToeGame() {
   return (
@@ -9,7 +9,9 @@ function TicTacToeGame() {
       <h4 className="sr-only">틱택토 게임</h4>
       <Stack gap={16}>
         <Board />
-        <History />
+        <Stack vertical>
+          <History />
+        </Stack>
       </Stack>
     </div>
   );
---
+++ src/learn/components/ticTacToe/Info.jsx
@@ -0,0 +1,7 @@
+import styles from './Info.module.css';
+
+function Info() {
+  return <div className={styles.component}>다음 플레이어 ⏺</div>;
+}
+
+export default Info;
