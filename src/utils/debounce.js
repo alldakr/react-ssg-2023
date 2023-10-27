8202eb6277a7c196a353459e6295f6226c4b356b
yamoo9 <yamoo9@naver.com>
2023-10-27 19:22:53 +0900

    디바운스 유틸리티 함수 추가

---
+++ src/utils/debounce.js
@@ -0,0 +1,13 @@
+// 사용자 입력이 멈춘 후 0.4초 동안 아무런 동작이 없다면?
+// 그 때 콜백 함수 실행
+function debounce(callback, timeout = 400 /* ms */) {
+  let clearId;
+
+  // JavaScript 클로저 활용
+  return (...args) => {
+    clearTimeout(clearId);
+    clearId = setTimeout(callback.bind(null, ...args), timeout);
+  };
+}
+
+export default debounce;
